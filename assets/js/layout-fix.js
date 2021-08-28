bad1a3e5d4b770a4857c542235f61ed42fe8ae78
jineshnagori <jineshn21@gmail.com>
2021-08-28 15:33:16 +0530

    pc and mobile header

--- assets/js/layout-fix.js
+++
@@ -1,8 +0,0 @@
-    $(window).on('scroll', function () {
-        if ($(this).scrollTop() > 400) {
-           $("header").addClass("fixed");
-        } else {
-            $("header").removeClass("fixed");
-        }
-    });
-
